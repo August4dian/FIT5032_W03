fcf3e8de49ae05df5cf447cec51c26d962fc4701
August4dian <54160458+August4dian@users.noreply.github.com>
2019-08-16 11:47:56 +1000

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIT5032_Week03.Tests.HellowWorld
+{
+    class Hello
+    {
+        public String GetHello()
+        {
+            return "Hellow World!"; 
+        }
+    }
+}
